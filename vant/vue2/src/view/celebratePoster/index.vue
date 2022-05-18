b2e7e1e1d6ebe8582fc28984ce53fffa0a29e7db
Hua，Ye（华烨） <huay@maxwealthfund.com>
2022-05-18 15:04:55 +0800

    20220518 15:04

--- vant/vue2/src/view/celebratePoster/index.vue
+++ vant/vue2/src/view/celebratePoster/index.vue
@@ -9,21 +9,49 @@
     <van-row type="flex">
         <van-col>
             <van-sidebar v-model="activeKey" >
+                <van-sidebar-item title="迅利A(波行版)" />
+                <van-sidebar-item title="迅利A(通用)" />
+                <van-sidebar-item title="迅利E(通用)" />
+                <van-sidebar-item title="开泰C" />
+                <van-sidebar-item title="开泰A" />
+                <van-sidebar-item title="宁旭稳进2期" />
+                <van-sidebar-item title="华嘉" />
                 <van-sidebar-item title="成长远航主代码" />
                 <!-- <van-sidebar-item title="主代码爆单" /> -->
                 <van-sidebar-item title="成长远航AC代码" />
                 <!-- <van-sidebar-item title="AC份额爆单" /> -->
                 <van-sidebar-item title="成长远航销量王者榜" />
-                <van-sidebar-item title="迅利A(波行版)" />
-                <van-sidebar-item title="迅利A(通用)" />
-                <van-sidebar-item title="迅利E(通用)" />
-                <van-sidebar-item title="宁旭稳进2期" />
-                <van-sidebar-item title="开泰C" />
-                <van-sidebar-item title="开泰A" />
-                <van-sidebar-item title="华嘉" />
             </van-sidebar>
         </van-col>
         <van-col v-show="activeKey===0" span="16" >
+            <van-button class="ml-sm mt-sm" type="info" @click="goEdit('xunliNew3')">前往生成</van-button>
+             <img class="img-container" src="../../static/xunliNew3.jpg">
+        </van-col>
+        <van-col v-show="activeKey===1" span="16" >
+            <van-button class="ml-sm mt-sm" type="info" @click="goEdit('xunliANew4')">前往生成</van-button>
+             <img class="img-container" src="../../static/xunliANew4.jpg">
+        </van-col>
+        <van-col v-show="activeKey===2" span="16" >
+            <van-button class="ml-sm mt-sm" type="info" @click="goEdit('xunliENew1')">前往生成</van-button>
+             <img class="img-container" src="../../static/xunliENew1.jpg">
+        </van-col>
+        <van-col v-show="activeKey===3" span="16" >
+            <van-button class="ml-sm mt-sm" type="info" @click="goEdit('kaitai')">前往生成</van-button>
+             <img class="img-container" src="../../static/kaitai.jpg">
+        </van-col>
+        <van-col v-show="activeKey===4" span="16" >
+            <van-button class="ml-sm mt-sm" type="info" @click="goEdit('kaitaiA')">前往生成</van-button>
+             <img class="img-container" src="../../static/kaitaiA.jpg">
+        </van-col>
+        <van-col v-show="activeKey===5" span="16" >
+            <van-button class="ml-sm mt-sm" type="info" @click="goEdit('ningxuwenjin3')">前往生成</van-button>
+             <img class="img-container" src="../../static/ningxuwenjin3.jpg">
+        </van-col>
+        <van-col v-show="activeKey===6" span="16" >
+            <van-button class="ml-sm mt-sm" type="info" @click="goEdit('huajia')">前往生成</van-button>
+             <img class="img-container" src="../../static/huajia.jpg">
+        </van-col>
+        <van-col v-show="activeKey===7" span="16" >
             <van-button class="ml-sm mt-sm" type="info" @click="goEdit('newMain')">前往生成</van-button>
              <img class="img-container" src="../../static/newMain.jpg">
         </van-col>
@@ -31,7 +59,7 @@
             <van-button class="ml-sm mt-sm" type="info" @click="goEdit('MainPieceBomb')">前往生成</van-button>
              <img class="img-container" src="../../static/MainPieceBomb.jpg">
         </van-col> -->
-        <van-col v-show="activeKey===1" span="16" >
+        <van-col v-show="activeKey===8" span="16" >
             <van-button class="ml-sm mt-sm" type="info" @click="goEdit('newAC')">前往生成</van-button>
              <img class="img-container" src="../../static/newAC.jpg">
         </van-col>
@@ -39,37 +67,9 @@
             <van-button class="ml-sm mt-sm" type="info" @click="goEdit('ACPieceBomb')">前往生成</van-button>
              <img class="img-container" src="../../static/ACPieceBomb.jpg">
         </van-col> -->
-        <van-col v-show="activeKey===2" span="16" >
+        <van-col v-show="activeKey===9" span="16" >
             <van-button class="ml-sm mt-sm" type="info" @click="goEdit('saleTop')">前往生成</van-button>
              <img class="img-container" src="../../static/saleTop.jpg">
-        </van-col>
-        <van-col v-show="activeKey===3" span="16" >
-            <van-button class="ml-sm mt-sm" type="info" @click="goEdit('xunliNew3')">前往生成</van-button>
-             <img class="img-container" src="../../static/xunliNew3.jpg">
-        </van-col>
-        <van-col v-show="activeKey===4" span="16" >
-            <van-button class="ml-sm mt-sm" type="info" @click="goEdit('xunliANew4')">前往生成</van-button>
-             <img class="img-container" src="../../static/xunliANew4.jpg">
-        </van-col>
-        <van-col v-show="activeKey===5" span="16" >
-            <van-button class="ml-sm mt-sm" type="info" @click="goEdit('xunliENew1')">前往生成</van-button>
-             <img class="img-container" src="../../static/xunliENew1.jpg">
-        </van-col>
-        <van-col v-show="activeKey===6" span="16" >
-            <van-button class="ml-sm mt-sm" type="info" @click="goEdit('ningxuwenjin3')">前往生成</van-button>
-             <img class="img-container" src="../../static/ningxuwenjin3.jpg">
-        </van-col>
-        <van-col v-show="activeKey===7" span="16" >
-            <van-button class="ml-sm mt-sm" type="info" @click="goEdit('kaitai')">前往生成</van-button>
-             <img class="img-container" src="../../static/kaitai.jpg">
-        </van-col>
-        <van-col v-show="activeKey===8" span="16" >
-            <van-button class="ml-sm mt-sm" type="info" @click="goEdit('kaitaiA')">前往生成</van-button>
-             <img class="img-container" src="../../static/kaitaiA.jpg">
-        </van-col>
-        <van-col v-show="activeKey===9" span="16" >
-            <van-button class="ml-sm mt-sm" type="info" @click="goEdit('huajia')">前往生成</van-button>
-             <img class="img-container" src="../../static/huajia.jpg">
         </van-col>
     </van-row>
   </div>
