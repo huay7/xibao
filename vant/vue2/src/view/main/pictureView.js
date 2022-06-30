20da230f73ca0fa43dc06bd56de84978a79eca44
Hua，Ye（华烨） <huay@maxwealthfund.com>
2022-06-30 10:44:04 +0800

    20220630 10:44

---
+++ vant/vue2/src/view/main/pictureView.js
@@ -0,0 +1,16 @@
+const param = {
+    imgUrl:'https://img01.yzcdn.cn/vant/cat.jpeg'
+}
+
+const pageM = {
+    id:"100",
+    title:'喜报',
+    model:'pictureview',
+    share:{
+      title:'永赢喜报制作',
+      desc:'',
+      imgUrl:''
+    },
+    param
+}
+export {pageM}
---
+++ vant/vue2/src/view/pictureView/index.vue
@@ -0,0 +1,31 @@
+<template>
+   <div>
+    <van-image
+        width="100%"
+        height="100%"
+        :src="this.param.imgUrl"
+     />
+   </div>
+</template>
+
+<script>
+export default {
+    props:{
+        param:[]
+    },
+   data () {
+      return {
+
+      };
+   },
+
+   components: {},
+
+   computed: {},
+
+   methods: {}
+}
+</script>
+<style>
+
+</style>
