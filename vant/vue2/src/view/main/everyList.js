bdfbf6dcd8f44f242c811974bc06617b8a9450a5
huangwenbo <1787742075@qq.com>
2022-07-13 22:59:32 +0800

    data

--- vant/vue2/src/view/main/everyList.js
+++ vant/vue2/src/view/main/everyList.js
@@ -62,36 +62,13 @@ const blockB = {
   type: 'prolist',
   productlist: [
     {
-      title: '热销',
+      title: '股债观点',
       list:[
         {
           type: 'img',
           title: '永赢成长领航A（010562）定投半年版',
           desc: '描述内容',
           img:'https://ewa-media.oss-cn-shanghai.aliyuncs.com/media/innernew/2022052003/%E5%AE%9A%E6%8A%95%E6%8E%A8%E8%8D%90-%E6%B0%B8%E8%B5%A2%E6%88%90%E9%95%BF%E9%A2%86%E8%88%AAA%EF%BC%88%E5%91%A8%E5%AE%9A%E6%8A%95%2B%E6%9C%88%E5%AE%9A%E6%8A%95%E7%89%88%EF%BC%890806.png'
-        },
-        {
-          type: 'img',
-          title: '永赢华嘉信用债A（010092）日/周/月定投测算版',
-          desc: '描述内容',
-          img:'https://ewa-media.oss-cn-shanghai.aliyuncs.com/media/innernew/2022052003/%E5%AE%9A%E6%8A%95%E6%8E%A8%E8%8D%90-%E6%B0%B8%E8%B5%A2%E5%8D%8E%E5%98%89%E4%BF%A1%E7%94%A8%E5%80%BAA%EF%BC%88%E5%B7%B2%E5%AE%A1%E6%A0%B8%EF%BC%89.jpg'
-        },
-      ]
-    },
-    {
-      title: '权益',
-      list:[
-        {
-          type: 'img',
-          title: '永赢成长领航A（010562）定投半年版',
-          desc: '描述内容',
-          img:'https://ewa-media.oss-cn-shanghai.aliyuncs.com/media/innernew/2022052003/%E5%AE%9A%E6%8A%95%E6%8E%A8%E8%8D%90-%E6%B0%B8%E8%B5%A2%E6%88%90%E9%95%BF%E9%A2%86%E8%88%AAA%EF%BC%88%E5%91%A8%E5%AE%9A%E6%8A%95%2B%E6%9C%88%E5%AE%9A%E6%8A%95%E7%89%88%EF%BC%890806.png'
-        },
-        {
-          type: 'img',
-          title: '永赢华嘉信用债A（010092）日/周/月定投测算版',
-          desc: '描述内容',
-          img:'https://ewa-media.oss-cn-shanghai.aliyuncs.com/media/innernew/2022052003/%E5%AE%9A%E6%8A%95%E6%8E%A8%E8%8D%90-%E6%B0%B8%E8%B5%A2%E5%8D%8E%E5%98%89%E4%BF%A1%E7%94%A8%E5%80%BAA%EF%BC%88%E5%B7%B2%E5%AE%A1%E6%A0%B8%EF%BC%89.jpg'
         },
       ]
     },
@@ -213,7 +190,7 @@ const blockE = {
   type: 'prolist',
   productlist: [
     {
-      title: '热销',
+      title: '月度陪伴',
       list:[
         {
           type: 'img',
@@ -230,24 +207,7 @@ const blockE = {
       ]
     },
     {
-      title: '权益',
-      list:[
-        {
-          type: 'img',
-          title: '永赢成长领航A（010562）定投半年版',
-          desc: '描述内容',
-          img:'https://ewa-media.oss-cn-shanghai.aliyuncs.com/media/innernew/2022052003/%E5%AE%9A%E6%8A%95%E6%8E%A8%E8%8D%90-%E6%B0%B8%E8%B5%A2%E6%88%90%E9%95%BF%E9%A2%86%E8%88%AAA%EF%BC%88%E5%91%A8%E5%AE%9A%E6%8A%95%2B%E6%9C%88%E5%AE%9A%E6%8A%95%E7%89%88%EF%BC%890806.png'
-        },
-        {
-          type: 'img',
-          title: '永赢华嘉信用债A（010092）日/周/月定投测算版',
-          desc: '描述内容',
-          img:'https://ewa-media.oss-cn-shanghai.aliyuncs.com/media/innernew/2022052003/%E5%AE%9A%E6%8A%95%E6%8E%A8%E8%8D%90-%E6%B0%B8%E8%B5%A2%E5%8D%8E%E5%98%89%E4%BF%A1%E7%94%A8%E5%80%BAA%EF%BC%88%E5%B7%B2%E5%AE%A1%E6%A0%B8%EF%BC%89.jpg'
-        },
-      ]
-    },
-    {
-      title: '固收',
+      title: '专户',
       list:[
         {
           type: 'img',
